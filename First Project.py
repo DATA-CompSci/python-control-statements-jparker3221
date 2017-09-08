423dddc9b159eb1e9496b7f28d000fc244f67d7e
jparker3221 <jparker3221@stu.neisd.net>
2017-09-08 15:03:39 -0500

    finished project

---
+++ First Project.py
@@ -0,0 +1,12 @@
+
+def method(x):
+        if x == 1:
+            print "hi!"
+        elif x == 2:
+            print "welcome to my code!"
+            x = 0
+x = 1
+while x > 0:
+    method(x)
+    x += 1
+    # method with and if statement and a while/for loop
